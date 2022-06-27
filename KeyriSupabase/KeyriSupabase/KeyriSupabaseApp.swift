668ebc617ea5de00762f50e1fe80d81aa003cc3a
Aditya Malladi <aditya@keyri.co>
2022-06-27 16:46:04 -0400

    init

---
+++ KeyriSupabase/KeyriSupabase/KeyriSupabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyriSupabaseApp.swift
+//  KeyriSupabase
+//
+//  Created by Aditya Malladi on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct KeyriSupabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyriSupabase/Pods/Target Support Files/Pods-KeyriSupabase-KeyriSupabaseUITests/Pods-KeyriSupabase-KeyriSupabaseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KeyriSupabase_KeyriSupabaseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KeyriSupabase_KeyriSupabaseUITests
+@end
